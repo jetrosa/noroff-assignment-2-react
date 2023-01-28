18a38622ffed39aec77cdb598727708568966d29
jetrosa <jetro.saarti@gmail.com>
2023-01-28 22:13:44 +0200

    blank img replaces white spaces and invalid chars

--- src/components/Translate/Translated.jsx
+++ src/components/Translate/Translated.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+const imgPx = 60;
 
 //Translation using sign images
 /**
@@ -9,17 +10,36 @@ const Translated = ({ input }) => {
   const [translationImages, setTranslationsImages] = useState([]);
 
   useEffect(() => {
+    const createBlankImage = (width, height) => {
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+
+      const ctx = canvas.getContext("2d");
+      ctx.fillStyle = "rgba(0, 0, 0, 0)";
+      ctx.fillRect(0, 0, width, height);
+
+      const img = new Image(width, height);
+      img.src = canvas.toDataURL();
+
+      return (
+        <img src={canvas.toDataURL()} alt={""} width={input} height={input} />
+      );
+    };
+
     const imgArray = [];
     for (let i = 0; i < input.length; i++) {
-      if (input.charAt(i) !== " ") {
+      if (input.charAt(i).match(/[a-z]/i) !== null) {
         imgArray.push(
           <img
             src={`/img/signs/${input.charAt(i).toLowerCase()}.png`}
             alt={input.charAt(i)}
-            width={60}
-            height={60}
+            width={imgPx}
+            height={imgPx}
           />
         );
+      } else {
+        imgArray.push(createBlankImage(imgPx, imgPx));
       }
     }
     setTranslationsImages(imgArray);
